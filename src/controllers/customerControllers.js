096b3f53f99fdebdffcd208ed1902b82aec497e9
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-12-12 22:58:32 -0300

    feat: get customer by id

--- src/controllers/customerControllers.js
+++ src/controllers/customerControllers.js
@@ -15,3 +15,28 @@ export async function getCustomers(req, res) {
 		res.sendStatus(500);
 	}
 }
+
+export async function getCustomerById(req, res) {
+	const { id } = req.params;
+
+	try {
+		const customers = await connection.query(
+			`
+        SELECT *
+        FROM customers
+		WHERE id = $1
+        `,
+			[id]
+		);
+
+		if (customers.rows.length === 0) {
+			res.sendStatus(404);
+			return;
+		}
+
+		res.send(customers.rows[0]);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+}
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { getCustomers } from '../controllers/customerControllers.js';
+import {
+	getCustomers,
+	getCustomerById,
+} from '../controllers/customerControllers.js';
 
 const router = Router();
 
 router.get('/customers', getCustomers);
+
+router.get('/customers/:id', getCustomerById);
 
 export default router;
