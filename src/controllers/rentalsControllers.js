a3b997539d67cc06d360beb461558dcb27742a49
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-12-13 15:29:49 -0300

    fix: included gameId query string in get rentals

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,44 +1,16 @@
 import { connection } from '../database/db.js';
 
 export async function getRentals(req, res) {
-	const json = `SELECT json_build_object(
-		'id', rentals.id,
-		'customerId', rentals."customerId",
-		'gameId', rentals."gameId",
-		'rentDate', rentals."rentDate",
-		'daysRented', rentals."daysRented",
-		'returnDate', rentals."returnDate",
-		'originalPrice', rentals."originalPrice",
-		'delayFee', rentals."delayFee",
-		'customer', json_build_object(
-			'id', customers.id, 'name', customers.name),
-			
-		'game', json_build_object(
-			'id', games.id, 
-			'name', games.name,
-			'categoryId', games."categoryId",
-			'categoryName', categories.name)
-			)
-	FROM rentals JOIN customers
-		ON rentals."customerId" = customers.id
-		JOIN games
-		ON rentals."gameId" = games.id
-		JOIN categories
-		ON games."categoryId" = categories.id
-		;
-	`;
-	const queryPrevious = `
-	SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName"
-	FROM rentals JOIN customers
-	ON rentals."customerId" = customers.id
-	JOIN games
-	ON rentals."gameId" = games.id
-	JOIN categories
-	ON games."categoryId" = categories.id
-	;
-	`;
-	try {
-		const rentals = await connection.query(`SELECT 
+	const customerId = req.query.customerId;
+	const gameId = req.query.gameId;
+
+	const queryExists = customerId || gameId;
+	const templateString = `rentals."${
+		customerId ? 'customerId' : 'gameId'
+	}" = ${customerId ? customerId : gameId}`;
+
+	const stringWithQuery = `
+	SELECT 
 			rentals.id,
 			 rentals."customerId",
 			 rentals."gameId",
@@ -61,8 +33,39 @@ export async function getRentals(req, res) {
 			ON rentals."gameId" = games.id
 			JOIN categories
 			ON games."categoryId" = categories.id
-			;
-		`);
+			WHERE ${templateString};
+	`;
+
+	const stringNoQuery = `SELECT 
+	rentals.id,
+	 rentals."customerId",
+	 rentals."gameId",
+	 rentals."rentDate",
+	 rentals."daysRented",
+	 rentals."returnDate",
+	 rentals."originalPrice",
+	 rentals."delayFee",
+	 json_build_object('id', customers.id, 'name', customers.name)  AS "customer",
+	json_build_object(
+		'id', games.id, 
+		'name', games.name,
+		'categoryId', games."categoryId",
+		'categoryName', categories.name
+		) AS "game"
+		
+FROM rentals JOIN customers
+	ON rentals."customerId" = customers.id
+	JOIN games
+	ON rentals."gameId" = games.id
+	JOIN categories
+	ON games."categoryId" = categories.id
+	;
+`;
+
+	try {
+		const rentals = await connection.query(
+			queryExists ? stringWithQuery : stringNoQuery
+		);
 		console.log(rentals.rows);
 
 		res.send(rentals.rows);
