941e6bc1e62938d4a59268b064df19fd13e9151c
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-12-13 15:11:54 -0300

    feat: sending correct data in get rentals

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,11 +1,69 @@
 import { connection } from '../database/db.js';
 
 export async function getRentals(req, res) {
+	const json = `SELECT json_build_object(
+		'id', rentals.id,
+		'customerId', rentals."customerId",
+		'gameId', rentals."gameId",
+		'rentDate', rentals."rentDate",
+		'daysRented', rentals."daysRented",
+		'returnDate', rentals."returnDate",
+		'originalPrice', rentals."originalPrice",
+		'delayFee', rentals."delayFee",
+		'customer', json_build_object(
+			'id', customers.id, 'name', customers.name),
+			
+		'game', json_build_object(
+			'id', games.id, 
+			'name', games.name,
+			'categoryId', games."categoryId",
+			'categoryName', categories.name)
+			)
+	FROM rentals JOIN customers
+		ON rentals."customerId" = customers.id
+		JOIN games
+		ON rentals."gameId" = games.id
+		JOIN categories
+		ON games."categoryId" = categories.id
+		;
+	`;
+	const queryPrevious = `
+	SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName"
+	FROM rentals JOIN customers
+	ON rentals."customerId" = customers.id
+	JOIN games
+	ON rentals."gameId" = games.id
+	JOIN categories
+	ON games."categoryId" = categories.id
+	;
+	`;
 	try {
-		const rentals = await connection.query(`
-        SELECT *
-        FROM rentals;
-        `);
+		const rentals = await connection.query(`SELECT 
+			rentals.id,
+			 rentals."customerId",
+			 rentals."gameId",
+			 rentals."rentDate",
+			 rentals."daysRented",
+			 rentals."returnDate",
+			 rentals."originalPrice",
+			 rentals."delayFee",
+			 json_build_object('id', customers.id, 'name', customers.name)  AS "customer",
+			json_build_object(
+				'id', games.id, 
+				'name', games.name,
+				'categoryId', games."categoryId",
+				'categoryName', categories.name
+				) AS "game"
+				
+		FROM rentals JOIN customers
+			ON rentals."customerId" = customers.id
+			JOIN games
+			ON rentals."gameId" = games.id
+			JOIN categories
+			ON games."categoryId" = categories.id
+			;
+		`);
+		console.log(rentals.rows);
 
 		res.send(rentals.rows);
 	} catch (err) {
